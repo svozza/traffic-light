c097b0a1df6f38800e01990c740befd4b5f90227
Rob Hitt <robhitt@gmail.com>
2017-03-20 16:26:49 -0400

    working clickable traffic light

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+// Setting Variables
+var allLights = document.getElementsByClassName("light")
+var yellowID = document.getElementById('yellow');
+var greenID = document.getElementById('green');
+var redID = document.getElementById('red');
+
+// Adding click event listeners to each traffic light color id
+for (var i = 0; i < allLights.length; i++){
+  allLights[i].addEventListener("click", displayLight);
+}
+
+// This function turns off current light (all lights) and turns on just the the selected light
+function displayLight(e){
+  lightsOff();
+
+  if (e.target.id === 'red'){
+
+    console.log('red clicked');
+    redID.classList.add('light-visible');
+
+  } else if (e.target.id === 'yellow'){
+
+    console.log('yellow clicked');
+    yellowID.classList.add('light-visible');
+
+  } else {
+
+    console.log('green clicked');
+    greenID.classList.add('light-visible');
+  }
+}
+
+// helper function to turn off lights
+function lightsOff() {
+  for (var i = 0; i < allLights.length; i++) {
+    allLights[i].classList.remove("light-visible");
+  }
+}
